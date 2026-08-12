004ea730b88178bba6646a7f8c375bee17d4c9bf
sivaraman1490-lgtm <sivaraman1490@gmail.com>
2026-08-12 09:50:38 +0530

    Add files via upload
    Week2day1class assignment commit

---
+++ Week2day1/printoddnumbers.js
@@ -0,0 +1,12 @@
+function oddNumbers()
+{
+    for(let i=1;i<=25;i++)
+    {
+        if(i%2!==0)
+        {
+            console.log(i);
+        }
+    }
+    
+}
+oddNumbers();
---
+++ Week2day1/string.js
@@ -0,0 +1,9 @@
+
+/*String literal*/
+
+let companyname ='Test Leaf';
+let firstname ='Test Leaf';
+
+console.log(companyname === firstname);
+console.log(companyname == firstname);
+
